84d3c706b6f3474adf5b06df5cf06d5cae796eeb
anhquoctran <aquoc.hue@outlook.com>
2018-04-01 16:02:19 +0700

    first init

---
+++ icon_path_const.py
@@ -0,0 +1,13 @@
+class IconPathConstants():
+    
+    NO_SIGNAL = "./icon_set/no_icon"
+    WEAK_SIGNAL = "./icon_set/signal_1.bmp"
+    LOW_SIGNAL = "./icon_set/signal_2.bmp"
+    MEDIUM_SIGNAL = "./icon_set/signal_3.bmp"
+    HIGH_SIGNAL = "./icon_set/signal_4.bmp"
+    SETTING = "./icon_set/home.bmp"
+    WIFI_ON = "./icon_set/wifi1.bmp"
+
+    def __init__(self, *args):
+        super(IconPathConstants, self).__init__(*args)
+        
---
+++ recognition.py
@@ -0,0 +1,7 @@
+
+class Recognition(object):
+    
+    
+    def __init__(self, *args):
+        super(Recognition, self).__init__(*args))
+        
